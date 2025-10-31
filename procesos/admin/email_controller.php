8dd774bf27dd3f4bec5e6f3ece7db7c7fbe08572
andrescuastumal <andrescuastumal@gmail.com>
2025-10-31 08:13:26 -0500

    fixDirEmailController

--- procesos/admin/email_controller.php
+++ procesos/admin/email_controller.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../../includes/conexion.php';
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
