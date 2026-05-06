9a51b41ce6a3c37b88b20d5cf7ad04e3ab217041
andrescuastumal <andrescuastumal@gmail.com>
2026-05-06 11:07:18 -0500

    gr_capacitacion

--- vistas/dashboard.php
+++ vistas/dashboard.php
@@ -45,7 +45,7 @@ include '../includes/header.php'; // Incluye el encabezado HTML
                         <i class="bi bi-calendar-check text-primary fs-1"></i>
                         <h5 class="card-title">Reporte Año <?= date('Y', strtotime('-1 year') ) ?></h5>
                         <p class="card-text">Módulo para reportar información relacionada con gestión de residuos generados en atención en salud y otras activiades para el año <?= date('Y', strtotime('-1 year') ) ?>.</p>
-                        <?php if (in_array($rol, ['generador']) && $fecha_actual <= $fecha_limite):  ?>
+                        <?php if (in_array($rol, ['generador']) && $fecha_actual >= $fecha_limite):  ?>
                             <a href="generador/reporte_mensual_view.php" class="btn btn-sm btn-outline-primary mt-2">Acceder</a>
                         <?php endif; ?>
                     </div>
